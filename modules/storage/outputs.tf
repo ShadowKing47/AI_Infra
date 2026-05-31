d69118588eed5cbc0734c79130c5680df5610a59
Noel <noel@foxo.club>
2026-05-31 19:15:16 +0530

    Cleaning and refactoring

--- modules/storage/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_ids" {
-  description = "Map of logical name → S3 bucket ID."
-  value       = { for k, v in aws_s3_bucket.this : k => v.id }
-}
-
-output "bucket_arns" {
-  description = "Map of logical name → S3 bucket ARN."
-  value       = { for k, v in aws_s3_bucket.this : k => v.arn }
-}
